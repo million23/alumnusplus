0abd710fe338d9e40b93f15160c10498b36834eb
Gerald Chavez <chavezgerald23@gmail.com>
2025-02-27 19:34:41 +0800

    nagpaupdate si gab ng picture

--- pages/util/about-us.tsx
+++ pages/util/about-us.tsx
@@ -1,13 +1,12 @@
 import { AnimPageTransition } from "@/lib/animations";
-import Footer from "@/components/Footer";
-import Image from "next/image";
-import { NextPage } from "next";
 import { motion } from "framer-motion";
+import type { NextPage } from "next";
+import Image from "next/image";
 
 const thePeople = [
 	{
 		name: "Gabrielle D. Napoto",
-		img: "/profile/gabie.jpg",
+		img: "/profile/gabie-new.webp",
 		position: "Project Manager",
 	},
 	{
@@ -75,14 +74,14 @@ const AboutPage: NextPage = () => {
 				initial="initial"
 				animate="animate"
 				exit="exit"
-				className="relative min-h-screen w-full pt-16 lg:pt-24 pb-36"
+				className="relative pt-16 pb-36 w-full min-h-screen lg:pt-24"
 			>
-				<div className="flex flex-col lg:flex-row justify-between gap-8">
-					<div className="w-full lg:w-5/12 flex flex-col justify-center">
-						<h1 className="text-3xl lg:text-4xl font-bold leading-9 text-primary pb-4">
+				<div className="flex flex-col gap-8 justify-between lg:flex-row">
+					<div className="flex flex-col justify-center w-full lg:w-5/12">
+						<h1 className="pb-4 text-3xl font-bold leading-9 lg:text-4xl text-primary">
 							About Us
 						</h1>
-						<p className="font-normal text-base leading-6 text-base-content ">
+						<p className="text-base font-normal leading-6 text-base-content">
 							Come on in and discover what opportunities await you at Wicket—a
 							progressive web career platform for Hunters and Provisioners (in
 							Metro Manila). By establishing a platform where job providers and
@@ -90,9 +89,9 @@ const AboutPage: NextPage = () => {
 							steady increase of the employment rate in the Philippines.
 						</p>
 					</div>
-					<div className="w-full lg:w-8/12 relative">
+					<div className="relative w-full lg:w-8/12">
 						<Image
-							className="w-full h-full object-cover rounded-btn"
+							className="object-cover w-full h-full rounded-btn"
 							src="/profile/groupbg.png"
 							alt="A group of People"
 							fill
@@ -100,12 +99,12 @@ const AboutPage: NextPage = () => {
 					</div>
 				</div>
 
-				<div className="flex lg:flex-row flex-col justify-between gap-8 pt-12">
-					<div className="w-full lg:w-5/12 flex flex-col justify-center">
-						<h1 className="text-3xl lg:text-4xl font-bold leading-9 text-primary pb-4">
+				<div className="flex flex-col gap-8 justify-between pt-12 lg:flex-row">
+					<div className="flex flex-col justify-center w-full lg:w-5/12">
+						<h1 className="pb-4 text-3xl font-bold leading-9 lg:text-4xl text-primary">
 							Our Story
 						</h1>
-						<p className="font-normal text-base leading-6 text-base-content ">
+						<p className="text-base font-normal leading-6 text-base-content">
 							Wicket is developed by Wicket Journeys - a band of college
 							students (from UCC) running on caffeine-injected three-hour
 							sleeps, dreams of making it to the end of the semester, and hopes
@@ -114,11 +113,11 @@ const AboutPage: NextPage = () => {
 						</p>
 					</div>
 					<div className="w-full lg:w-8/12 lg:pt-8">
-						<div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-2 lg:gap-2">
+						<div className="grid grid-cols-2 md:grid-cols-3 sm:grid-cols-2 lg:gap-2">
 							{thePeople.map((person, key: number) => (
 								<div
 									key={`person-${key + 1}`}
-									className="p-4 pb-6 flex justify-center flex-col items-center"
+									className="flex flex-col justify-center items-center p-4 pb-6"
 								>
 									<Image
 										className="md:block hidden object-cover object-center w-[100px] h-[100px] rounded-full"
@@ -134,10 +133,10 @@ const AboutPage: NextPage = () => {
 										width={100}
 										height={100}
 									/>
-									<p className="font-medium text-xl leading-5 mt-4 text-center">
+									<p className="mt-4 text-xl font-medium leading-5 text-center">
 										{person.name}
 										<br />
-										<span className="font-normal leading-none opacity-50 text-xs">
+										<span className="text-xs font-normal leading-none opacity-50">
 											{person.position}
 										</span>
 									</p>
